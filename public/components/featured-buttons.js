3bcf211ff4be1ddf3ee4e55ead563f9f958b3a72
Angelena Barclay <angelenabarclay@gmail.com>
2025-05-07 15:36:29 -0400

    added districts button

--- public/components/featured-buttons.js
+++ public/components/featured-buttons.js
@@ -8,6 +8,7 @@ class Featured extends HTMLElement {
         <div class="buttons">
           <img src="/images/buttons/nekoweborg007.gif" />
           <img src="/images/buttons/lain.gif" />
+          <a href="https://districts.nekoweb.org/" style="padding-right: 3px;"><img alt="Nekoweb Districts" src="https://districts.nekoweb.org/img/districts002.png"></a>
           <img src="/images/buttons/pictochat.gif" />
           <img src="/images/buttons/bvwac.gif" />
           <img src="/images/buttons/IMG_1816.gif" />
